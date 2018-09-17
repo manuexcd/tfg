ae05e149c142f5a33ac1b4be706fa1c5039b7886
manuexcd <manuexcd@gmail.com>
2018-09-17 12:18:34 +0200

    nuevos tests

--- src/test/java/com/uca/tfg/service/OrderLineServiceTest.java
+++ src/test/java/com/uca/tfg/service/OrderLineServiceTest.java
@@ -1,7 +1,9 @@
 package com.uca.tfg.service;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 import java.util.Arrays;
@@ -9,33 +11,48 @@ import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.uca.tfg.dao.OrderLineDAO;
 import com.uca.tfg.model.OrderLine;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class OrderLineServiceTest {
 	
 	@MockBean
+	private OrderLineDAO dao;
+	
+	@Autowired
 	private OrderLineManager service;
 	
 	@Test
 	public void testGetAllOrderLines() {
-		given(service.getAllOrderLines()).willReturn(Arrays.asList(new OrderLine()));
+		given(dao.findAll()).willReturn(Arrays.asList(new OrderLine()));
 		assertNotNull(service.getAllOrderLines());
 	}
 
 	@Test
 	public void testGetOrderLineById() {
-		given(service.getOrderLine(Mockito.anyLong())).willReturn(Optional.of(new OrderLine()).get());
-		assertNotNull(service.getOrderLine(Mockito.anyLong()));
+		given(dao.findById(anyLong())).willReturn(Optional.of(new OrderLine()));
+		assertNotNull(service.getOrderLine(anyLong()));
 	}
 
 	@Test
 	public void testOrderLineNotFound() {
-		given(service.getOrderLine(Mockito.anyLong())).willReturn(null);
-		assertNull(service.getOrderLine(Mockito.anyLong()));
+		given(dao.findById(anyLong())).willReturn(Optional.ofNullable(null));
+		assertNull(service.getOrderLine(anyLong()));
+	}
+	
+	@Test
+	public void testDeleteOrderLine() {
+		OrderLine orderLine = new OrderLine();
+		dao.save(orderLine);
+		long id = orderLine.getId();
+		service.deleteOrderLine(id);
+		assertFalse(dao.existsById(id));
 	}
 }
