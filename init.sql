c20e802e42b73d02880861911dc95078f5aeb655
Manuel Lara <Manuel Lara@manue-Latitude-E5570>
2019-01-12 17:35:46 +0100

    Init database

---
+++ init.sql
@@ -0,0 +1,21 @@
+INSERT INTO image VALUES(1, 'https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-fabricante-de-avatar-masculino.jpg');
+INSERT INTO image VALUES(2, 'https://images.vexels.com/media/users/3/145922/preview2/eb6591b54b2b6462b4c22ec1fc4c36ea-fabricante-de-avatar-femenino.jpg');
+INSERT INTO image VALUES(3, 'https://psmedia.playstation.com/is/image/psmedia/ps4-listing-thumb-01-ps4-eu-06sep16?$Icon$');
+INSERT INTO image VALUES(4, 'https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2018/08/27/15353813396346.jpg');
+INSERT INTO image VALUES(5, 'https://www.nintendo.com/switch/assets/images/switch/buy-now/bundle_color_console.jpg');
+
+
+INSERT INTO product VALUES(1, 'Playstation 4', true, 'Sony Playstation 4', 300, 100, 3);
+INSERT INTO product VALUES(2, 'Xbox One X', true, 'Microsoft Xbox One X', 200, 100, 4);
+INSERT INTO product VALUES(3, 'Nintento Switch', true, 'Nintento Switch', 300, 100, 5);
+
+INSERT INTO user_table VALUES(1, 'Plaza Algodonales 2', 'manuexcd@gmail.com', 'Manuel', '$2a$10$EXe.QQRGZnUZhnvWNG3kM.98JguEXFPaRsU5W//vJnDkbHBmVNSxu', '638489260', 'Lara', 1);
+INSERT INTO user_table VALUES(2, 'Santiago Bernabeu', 'antoniaruiz@gmail.com', 'Antonia', '$2a$10$AUV8yT8FCptRboeRJu/nR.VLBa5/Ec7NKfol5ANEqE9sLvQ9WW8mq', '638489261', 'Ruiz', 2);
+INSERT INTO user_table VALUES(3, 'Plaza Algodonales 1', 'email1@gmail.com', 'Luis', '$2a$10$EeF4dFAiksrtOHBstaGA4OQS/iROiVoIKWYr/iTzM/Vz2.q.yuaAW', '638489262', 'Ramírez', 1);
+INSERT INTO user_table VALUES(4, 'Santiago Bernabeu 2', 'email2@gmail.com', 'María', '$2a$10$INNO78YlHh.de9CdFFu3UuduSeY/Tj6OjgGJgjBljDB1Wb47oOsRK', '638489263', 'Ruiz', 2);
+INSERT INTO user_table VALUES(5, 'Plaza Algodonales 22', 'email3@gmail.com', 'Antonio', '$2a$10$E8CEixhclN/k60ncV4sjPuM5M6IhF8iv5ZIgPsF2gBg0BLnCpCSri', '638489264', 'Gómez', 1);
+INSERT INTO user_table VALUES(6, 'Santiago Bernabeu 22', 'email4@gmail.com', 'Ana', '$2a$10$71x4PKHlp27Zf0t.nT2dBetNSnVcrdidTkwVzvH1GO3I3jKhKjmNK', '638489265', 'Martínez', 2);
+INSERT INTO user_table VALUES(7, 'Plaza Algodonales 222', 'email5@gmail.com', 'Manuel', '$2a$10$u7QZvewDfvMA9EShvi3mleGq/B/M8Hqhb9Iwt81dQEwdSYSUCFFne', '638489266', 'Bueno', 1);
+INSERT INTO user_table VALUES(8, 'Santiago Bernabeu 222', 'email6@gmail.com', 'Antonia', '$2a$10$5IHzZ6gSOmHafOEMKNOKjubrEYq1skzyh.BBc0HTplNLeSbyq0tjq', '638489267', 'González', 2);
+INSERT INTO user_table VALUES(9, 'Plaza Algodonales 2222', 'email7@gmail.com', 'Gonzalo', '$2a$10$WUIZ8U4MhWyJABeywOX5YejKv./Selz.NHZHF1veX8tZDM.Pjs4He', '638489268', 'Soto', 1);
+INSERT INTO user_table VALUES(10, 'Santiago Bernabeu 2222', 'email8@gmail.com', 'Pepa', '$2a$10$DnNfDI4BXm464Tid9ZNCq.sY.XGHoCmuJtZO1RdmxKOhpxasT9eVG', '638489269', 'Fuentes', 2);
