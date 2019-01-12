e66fd92252ce973b00654498854df48a9e0dbfcd
Manuel Lara <Manuel Lara@manue-Latitude-E5570>
2019-01-12 17:34:54 +0100

    ModelMapper

--- src/main/java/com/uca/tfg/TfgApplication.java
+++ src/main/java/com/uca/tfg/TfgApplication.java
@@ -1,5 +1,6 @@
 package com.uca.tfg;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,4 +17,9 @@ public class TfgApplication {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+	
+	@Bean
+	public ModelMapper modelMapper() {
+	    return new ModelMapper();
+	}
 }
