0ae112116f41c163e4b18496ee8978db10649cbe
Manuel  Lara <manuexcd@gmail.com>
2019-09-05 17:10:29 +0200

    @component

--- src/main/java/com/uca/tfg/controller/ImageMapperImpl.java
+++ src/main/java/com/uca/tfg/controller/ImageMapperImpl.java
@@ -1,9 +1,12 @@
 package com.uca.tfg.controller;
 
+import org.springframework.stereotype.Component;
+
 import com.uca.tfg.dto.ImageDTO;
 import com.uca.tfg.mapper.GenericMapperImpl;
 import com.uca.tfg.model.Image;
 
+@Component
 public class ImageMapperImpl extends GenericMapperImpl<Image, ImageDTO> implements ImageMapper {
 
 	@Override
